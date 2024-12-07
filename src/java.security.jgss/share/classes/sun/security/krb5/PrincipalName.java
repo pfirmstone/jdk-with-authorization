802a97c27ffa4dd617dbdac8e4561e4a557165b0
pfirmstone <jini@zeus.net.au>
2024-12-07 20:19:53 +1000

    Revert "8344768: Consider removing "sun.security.krb5.autodeducerealm" system property"

--- src/java.security.jgss/share/classes/sun/security/krb5/PrincipalName.java
+++ src/java.security.jgss/share/classes/sun/security/krb5/PrincipalName.java
@@ -99,6 +99,7 @@ public class PrincipalName implements Cloneable {
      * TGS Name
      */
     public static final String TGS_DEFAULT_SRV_NAME = "krbtgt";
+    public static final int TGS_DEFAULT_NT = KRB_NT_SRV_INST;
 
     public static final char NAME_COMPONENT_SEPARATOR = '/';
     public static final char NAME_REALM_SEPARATOR = '@';
@@ -106,6 +107,7 @@ public class PrincipalName implements Cloneable {
 
     public static final String NAME_COMPONENT_SEPARATOR_STR = "/";
     public static final String NAME_REALM_SEPARATOR_STR = "@";
+    public static final String REALM_COMPONENT_SEPARATOR_STR = ".";
 
     private static final boolean NAME_CASE_SENSITIVE_IN_MATCH
             = "true".equalsIgnoreCase(
@@ -133,6 +135,12 @@ public class PrincipalName implements Cloneable {
     private final Realm nameRealm;      // not null
 
 
+    /**
+     * When constructing a PrincipalName, whether the realm is included in
+     * the input, or deduced from default realm or domain-realm mapping.
+     */
+    private final boolean realmDeduced;
+
     // cached default salt, not used in clone
     private transient String salt = null;
 
@@ -153,6 +161,7 @@ public class PrincipalName implements Cloneable {
         this.nameType = nameType;
         this.nameStrings = nameStrings.clone();
         this.nameRealm = nameRealm;
+        this.realmDeduced = false;
     }
 
     // Warning: called by NativeCreds.c
@@ -242,6 +251,7 @@ public class PrincipalName implements Cloneable {
         if (realm == null) {
             throw new IllegalArgumentException("Null realm not allowed");
         }
+        realmDeduced = false;
         nameRealm = realm;
         DerValue der;
         if (encoding == null) {
@@ -395,6 +405,9 @@ public class PrincipalName implements Cloneable {
             realm = Realm.parseRealmAtSeparator(name);
         }
 
+        // No realm info from parameter and string, must deduce later
+        realmDeduced = realm == null;
+
         switch (type) {
         case KRB_NT_SRV_HST:
             if (nameParts.length >= 2) {
@@ -424,8 +437,8 @@ public class PrincipalName implements Cloneable {
                                 hostName.toLowerCase(Locale.ENGLISH) + ".")) {
                             hostName = canonicalized;
                         }
-                    } catch (UnknownHostException e) {
-                        // not canonicalized, use old
+                    } catch (UnknownHostException | SecurityException e) {
+                        // not canonicalized or no permission to do so, use old
                     }
                     if (hostName.endsWith(".")) {
                         hostName = hostName.substring(0, hostName.length() - 1);
@@ -712,5 +725,9 @@ public class PrincipalName implements Cloneable {
         } catch (KrbException e) {
         }
         return result;
+    }
+
+    public boolean isRealmDeduced() {
+        return realmDeduced;
     }
 }
