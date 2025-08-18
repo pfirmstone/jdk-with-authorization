e377235bbcfd122308b4d171d65d5919aadc39b4
pfirmstone <jini@zeus.net.au>
2025-08-18 16:45:55 +1000

    SecurityManager & AccessController support for privileges and access control with VirtualThread's #46
    AccessControlContext instances are created early during JVM bootstrap, using a cache appears to have caused some ongoing gtest failures, that don't occur otherwise.   While it hasn't been confirmed as the cause, it is thought that the previous method of creating AccessControlContext instances should be used until the jvm has been fully initialized.

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -4773,20 +4773,32 @@ void java_security_AccessControlContext::serialize_offsets(SerializeClosure* f) 
 // Now it uses a build method, however we don't initialize the AccessControlContext
 // cache here.  The cache is initialized following VM init Phase 2.
 oop java_security_AccessControlContext::create(objArrayHandle context, bool isPrivileged, Handle privileged_context, TRAPS) {
+  assert(_isPrivileged_offset != 0, "offsets should have been initialized");
   Klass* klass = vmClasses::AccessControlContext_klass();
   // Ensure klass is initialized
   klass->initialize(CHECK_NULL);
-  JavaValue result(T_OBJECT);
-  JavaCallArguments args;
-  args.push_oop(context);
-  args.push_oop(privileged_context);
-  args.push_int(isPrivileged);
-  JavaCalls::call_static(&result,
-                         klass,
-                         vmSymbols::buildAccessControlContext_name(),
-                         vmSymbols::buildAccessControlContext_signature(),
-                         &args, CHECK_NULL);
-  return result.get_oop();
+  // Ensure object heap has been allocated before calling factory method.
+  if (Universe::is_fully_initialized()){ // AccessControlContext may be cached.
+      JavaValue result(T_OBJECT);
+      JavaCallArguments args;
+      args.push_oop(context);
+      args.push_oop(privileged_context);
+      args.push_int(isPrivileged);
+      JavaCalls::call_static(&result,
+                             klass,
+                             vmSymbols::buildAccessControlContext_name(),
+                             vmSymbols::buildAccessControlContext_signature(),
+                             &args, CHECK_NULL);
+      return result.get_oop();
+  } else { // Create a unique AccessControlContext instance.
+      // Allocate result
+      oop result = vmClasses::AccessControlContext_klass()->allocate_instance(CHECK_NULL);
+      // Fill in values
+      result->obj_field_put(_context_offset, context());
+      result->obj_field_put(_privilegedContext_offset, privileged_context());
+      result->bool_field_put(_isPrivileged_offset, isPrivileged);
+      return result;
+  }
 }
 
 
