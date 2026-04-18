5d7def3ddeb6f32c299fe5002e7996ca42b52ff5
pfirmstone <jini@zeus.net.au>
2026-04-19 09:33:43 +1000

    Update javaClasses.cpp
    Fix assertion error in tests, caused by duplicated information failing assertion.

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -3717,10 +3717,8 @@ oop java_lang_reflect_RecordComponent::create(InstanceKlass* holder, RecordCompo
   java_lang_reflect_RecordComponent::set_name(element(), component_name);
 
   Symbol* type = holder->constants()->symbol_at(component->descriptor_index());
-  Handle loader(THREAD, holder->class_loader());
-  Handle protection_domain(THREAD, holder->protection_domain());
   Handle component_type_h =
-    SystemDictionary::find_java_mirror_for_type(type, holder, loader, protection_domain, SignatureStream::NCDFError, CHECK_NULL);
+    SystemDictionary::find_java_mirror_for_type(type, holder, Handle(), Handle(), SignatureStream::NCDFError, CHECK_NULL);
   java_lang_reflect_RecordComponent::set_type(element(), component_type_h());
 
   Method* accessor_method = nullptr;
