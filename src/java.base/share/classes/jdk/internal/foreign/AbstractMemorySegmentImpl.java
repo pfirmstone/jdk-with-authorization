bc3099cf65070c7e059a54e36b75741c4f99ea10
pfirmstone <jini@zeus.net.au>
2026-04-21 07:47:46 +1000

    Add library-scoped NativeInvocationPermission checks at native invocation boundaries #118

--- src/java.base/share/classes/jdk/internal/foreign/AbstractMemorySegmentImpl.java
+++ src/java.base/share/classes/jdk/internal/foreign/AbstractMemorySegmentImpl.java
@@ -154,6 +154,10 @@ public abstract sealed class AbstractMemorySegmentImpl
     }
 
     private NativeMemorySegmentImpl reinterpretInternal(Class<?> callerClass, long newSize, MemorySessionImpl scope, Consumer<MemorySegment> cleanup) {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new NativeInvocationPermission("reinterpret"));
+        }
         Reflection.ensureNativeAccess(callerClass, MemorySegment.class, "reinterpret", false);
         Utils.checkNonNegativeArgument(newSize, "newSize");
         if (!isNative()) throw new UnsupportedOperationException("Not a native segment");
