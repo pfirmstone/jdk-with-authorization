7051d7b961e401091f523163bf24786889857d0a
pfirmstone <jini@zeus.net.au>
2026-04-21 21:16:37 +1000

    Add library-scoped NativeInvocationPermission checks at native invocation boundaries #118

--- src/java.base/share/classes/java/lang/foreign/SymbolLookup.java
+++ src/java.base/share/classes/java/lang/foreign/SymbolLookup.java
@@ -260,10 +260,19 @@ public interface SymbolLookup {
             JavaLangAccess javaLangAccess = SharedSecrets.getJavaLangAccess();
             // note: ClassLoader::findNative supports a null loader
             NativeLibraries nativeLibraries = javaLangAccess.nativeLibrariesFor(loader);
-            long addr = nativeLibraries.find(name);
-            return addr == 0L ? Optional.empty():
-                Optional.of(MemorySegment.ofAddress(addr)
+            Map.Entry<String, Long> libNameAddress = nativeLibraries.findLibraryNameAddress(name);
+            long addr = 0;
+            if (libNameAddress != null){
+                Long val = libNameAddress.getValue();
+                if (val != null) addr = val;
+            } 
+            if (addr == 0L){
+                return Optional.empty();
+            } else {
+                new NativeInvocationPermission(libNameAddress.getKey()).checkGuard(null);
+                return Optional.of(MemorySegment.ofAddress(addr)
                                 .reinterpret(loaderArena, null)); // restricted
+            }
         };
     }
 
@@ -365,9 +374,13 @@ public interface SymbolLookup {
             Objects.requireNonNull(name);
             if (Utils.containsNullChars(name)) return Optional.empty();
             long addr = library.find(name);
-            return addr == 0L ? Optional.empty():
-                Optional.of(MemorySegment.ofAddress(addr)
+            if (addr == 0L){
+                return Optional.empty();
+            } else {
+                new NativeInvocationPermission(library.name()).checkGuard(null);
+                return Optional.of(MemorySegment.ofAddress(addr)
                                 .reinterpret(libArena, null));  // restricted
+            }
         };
     }
 }
